46f3e79739ad309b1c62eef7cfa20bd53562d6cb
parikhshyamal1993 <55177728+parikhshyamal1993@users.noreply.github.com>
2021-06-24 15:32:01 +0530

    Delete FrontEndTest.py

--- HelpDesk/source/FrontEndTest.py
+++
@@ -1,7 +0,0 @@
-import requests
-Link = 'http://192.168.29.80:5000'
-query = "L&T Technology services"
-Outs = requests.get(Link+'/'+str(query))
-Title , FileOutput = Outs.json()
-
-print("Title  :" , Title)
